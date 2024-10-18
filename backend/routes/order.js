9ef2a9eb9b3baa6f6e1735587101efd59bd22b96
Asimpossible <asimgarasov@gmail.com>
2024-10-18 16:50:37 +0400

    Completed backend and added frontend folder

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -4,7 +4,6 @@ import { allOrders, deleteOrder, getMyOrders, getOrderDetails, newOrder, updateO
 
 const router = express.Router();
 
-
 router.route("/orders/new").post(isAuthenticatedUser, newOrder);
 router.route("/orders/:id").get(isAuthenticatedUser, getOrderDetails);
 router.route("/me/orders").get(isAuthenticatedUser, getMyOrders);
@@ -12,7 +11,5 @@ router.route("/me/orders").get(isAuthenticatedUser, getMyOrders);
 router.route("/admin/orders").get(isAuthenticatedUser, authorizeRoles('admin'), allOrders);
 router.route("/admin/orders/:id").put(isAuthenticatedUser, authorizeRoles('admin'), updateOrder);
 router.route("/admin/orders/:id").delete(isAuthenticatedUser, authorizeRoles('admin'), deleteOrder);
-
-
 
 export default router;
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteProduct, getProductDetails, getProducts, newProduct, updateProduct } from "../controllers/productController.js";
+import { createProductReview, deleteProduct, deleteReview, getProductDetails, getProductReviews, getProducts, newProduct, updateProduct } from "../controllers/productController.js";
 import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
 const router = express.Router();
 
@@ -8,5 +8,10 @@ router.route("/products/:id").get(getProductDetails);
 router.route("/admin/products").post(isAuthenticatedUser, authorizeRoles("admin"), newProduct);
 router.route("/admin/products/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct);
 router.route("/admin/products/:id").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+
+router.route("/reviews").get(isAuthenticatedUser, getProductReviews);
+router.route("/reviews").put(isAuthenticatedUser, createProductReview);
+
+router.route("/admin/reviews").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteReview);
 
 export default router; 
