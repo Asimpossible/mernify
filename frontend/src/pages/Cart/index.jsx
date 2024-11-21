84c5caaefb3e78919ea7ae99d30bb64f2e77acc1
Asimpossible <asimgarasov@gmail.com>
2024-11-22 02:28:52 +0400

    All Cart Functions Commpleted

--- frontend/src/pages/Cart/index.jsx
+++ frontend/src/pages/Cart/index.jsx
@@ -1,16 +1,54 @@
 import React from 'react'
 import { useSelector } from "react-redux"
 import { Link } from 'react-router-dom'
+import { useDispatch } from "react-redux"
+import { setCartItem, removeCartItem } from "../../redux/features/CartSlice";
 import MetaData from '../../layout/MetaData'
+
 
 const Index = () => {
     const { cartItems } = useSelector((state) => state.cart)
+    const dispatch = useDispatch()
+
+    const increaseQty = (item, quantity) => {
+        const newQty = quantity + 1;
+
+        if (newQty > item.stock) return
+
+        setItemToCart(item, newQty)
+    }
+
+    const decreaseQty = (item, quantity) => {
+        const newQty = item.quantity - 1;
+
+        if (newQty <= 0) return;
+
+        setItemToCart(item, newQty)
+    }
+
+    const setItemToCart = (item, newQty) => {
+        const cartItem = {
+            product: item?.product,
+            name: item?.name,
+            price: item?.price,
+            image: item?.image,
+            stock: item?.stock,
+            quantity: newQty
+        };
+
+        dispatch(setCartItem(cartItem))
+    }
+
+    const removeProduct = (id) => {
+        dispatch(removeCartItem(id))
+    }
+
     return (
         <>
             <MetaData title={'Your Cart'} />
             {cartItems?.length === 0 ? (
                 <>
-                    <h2 className="mt-5">Your Cart Is Empty ...</h2>
+                    <h2 className="mt-5 text-center">Your Cart Is Empty ...</h2>
                 </>
             ) : (
                 <>
@@ -18,13 +56,11 @@ const Index = () => {
                     <div className="row d-flex justify-content-between">
                         <div className="col-12 col-lg-8">
                             {cartItems?.map((item, index) => {
-                                console.log(item)
                                 return (
-                                    <>
-                                        <hr />
-                                        <div className="cart-item" data-key="product1">
-                                            <div className="row">
-                                                <div className="col-4 col-lg-3">
+                                    <div className='cart_product' key={index}>
+                                        <div className="cart-item" data-key="product1" key={index}>
+                                            <div className="row" >
+                                                <div className="col-4 col-lg-3" >
                                                     <img
                                                         src={item?.image}
                                                         alt={item?.name}
@@ -32,7 +68,7 @@ const Index = () => {
                                                         width="115"
                                                     />
                                                 </div>
-                                                <div className="col-5 col-lg-3">
+                                                <div className="col-5 col-lg-3" >
                                                     <Link to={`/product/${item?.product}`}> {item?.name} </Link>
                                                 </div>
                                                 <div className="col-4 col-lg-2 mt-4 mt-lg-0">
@@ -40,25 +76,28 @@ const Index = () => {
                                                 </div>
                                                 <div className="col-4 col-lg-3 mt-4 mt-lg-0">
                                                     <div className="stockCounter d-inline">
-                                                        <span className="btn btn-danger minus"> - </span>
+                                                        <span className="btn btn-danger minus" onClick={() => decreaseQty(item, item?.quantity)}> - </span>
                                                         <input
                                                             type="number"
                                                             className="form-control count d-inline"
                                                             value={item?.quantity}
                                                             readOnly="readOnly"
                                                         />
-                                                        <span className="btn btn-primary plus"> + </span>
+                                                        <span className="btn btn-primary plus" onClick={() => increaseQty(item, item?.quantity)}> + </span>
                                                     </div>
                                                 </div>
                                                 <div className="col-4 col-lg-1 mt-4 mt-lg-0">
-                                                    <i id="delete_cart_item" className="fa fa-trash btn btn-danger"></i>
+                                                    <i id="delete_cart_item" className="fa fa-trash btn btn-danger"
+                                                        onClick={
+                                                            () => {
+                                                                removeProduct(item?.product)
+                                                                console.log(item?.product)
+                                                            }}
+                                                    ></i>
                                                 </div>
                                             </div>
                                         </div>
-                                        <hr />
-
-
-                                    </>
+                                    </div>
                                 )
                             })}
                         </div>
@@ -66,8 +105,10 @@ const Index = () => {
                             <div id="order_summary">
                                 <h4>Order Summary</h4>
                                 <hr />
-                                <p>Subtotal: <span className="order-summary-values">8 (Units)</span></p>
-                                <p>Est. total: <span className="order-summary-values">$1499.97</span></p>
+                                <p>Subtotal: <span className="order-summary-values">
+                                    {cartItems.reduce((acc, item) => acc + item?.quantity, 0)} {" "}
+                                    (Units)</span></p>
+                                <p>Est. total: <span className="order-summary-values">$ {cartItems.reduce((acc, item) => acc + item.quantity * item.price, 0).toFixed(2)}</span></p>
                                 <hr />
                                 <button id="checkout_btn" className="btn btn-primary w-100">
                                     Check out
