d066e8046e2fe9e17839182696544cce0d2e3675
Asimpossible <asimgarasov@gmail.com>
2024-12-17 17:39:56 +0400

    Changes in Mongo DB URL

--- frontend/src/components/Admin/index.jsx
+++ frontend/src/components/Admin/index.jsx
@@ -37,7 +37,7 @@ const AdminLayout = ({ children }) => {
     ];
 
     return (
-        <>
+        <div>
             <div className="mt-2 mb-4 py-4">
                 <h2 className="text-center fw-bolder">Admin Dashboard</h2>
             </div>
@@ -48,7 +48,7 @@ const AdminLayout = ({ children }) => {
                 </div>
                 <div className="col-12 col-lg-8 user-dashboard">{children}</div>
             </div>
-        </>
+        </div>
     )
 }
 
